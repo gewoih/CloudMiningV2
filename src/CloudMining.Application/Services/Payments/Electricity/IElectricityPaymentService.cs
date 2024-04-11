24044e8964cca465d811c648dc31fcb2e7c80a51
solanova88 <grigmaks2014@yandex.ru>
2024-04-11 12:48:00 +0300

    feat: ElectricityPaymentService added

---
+++ src/CloudMining.Application/Services/Payments/Electricity/IElectricityPaymentService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudMining.Application.Services.Payments.Electricity
+{
+    public interface IElectricityPaymentService
+    {
+    }
+}
