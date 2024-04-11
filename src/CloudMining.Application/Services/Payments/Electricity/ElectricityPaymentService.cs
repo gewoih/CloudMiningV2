304eab716070df64cede90e2339a59dafd0d4887
solanova88 <grigmaks2014@yandex.ru>
2024-04-11 15:43:44 +0300

    feat: ElectricityPaymentService in progress

--- src/CloudMining.Application/Services/Payments/Electricity/ElectricityPaymentService.cs
+++ src/CloudMining.Application/Services/Payments/Electricity/ElectricityPaymentService.cs
@@ -4,14 +4,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CloudMining.Application.Models.Payments.Electricity;
+using CloudMining.Application.Services.Users;
+using CloudMining.Domain.Enums;
+using CloudMining.Domain.Models;
+using CloudMining.Domain.Models.Base;
 
 namespace CloudMining.Application.Services.Payments.Electricity
 {
     public sealed class ElectricityPaymentService : IElectricityPaymentService
     {
-        public async Task CreateAsync(ElectricityCredentials credentials)
-        {
+        private readonly IUserService _userService;
 
+        public ElectricityPaymentService(IUserService userService)
+        {
+            _userService = userService;
+        }
+        public async Task<ShareablePayment> CreateAsync(ElectricityCredentials credentials)
+        {
+            var allUsersIds = await _userService.GetAllUsersIdsAsync();
+            var paymentShares = new List<PaymentShare>();
+            foreach (var userId in allUsersIds)
+            {
+                var paymentShare = new PaymentShare
+                {
+                    UserId = userId,
+                    Amount = ,
+                    Percent = ,
+                    IsCompleted = false
+                };
+                paymentShares.Add(paymentShare);
+            }
+            var newPayment = new ShareablePayment
+            {
+                Amount = credentials.Amount,
+                Currency = ,
+                Type = PaymentType.Electricity,
+                IsCompleted = false,
+                PaymentShares = paymentShares
+            };
+            return newPayment;
         }
     }
 }
--- src/CloudMining.Application/Services/Payments/Electricity/IElectricityPaymentService.cs
+++ src/CloudMining.Application/Services/Payments/Electricity/IElectricityPaymentService.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CloudMining.Application.Models.Payments.Electricity;
+using CloudMining.Domain.Models;
 
 namespace CloudMining.Application.Services.Payments.Electricity
 {
     public interface IElectricityPaymentService
     {
-        Task CreateAsync(ElectricityCredentials credentials);
+        Task<ShareablePayment> CreateAsync(ElectricityCredentials credentials);
     }
 }
