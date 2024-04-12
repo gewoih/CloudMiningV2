1a22874b9cac6660638204f3d010d7bdaf72c33a
solanova88 <grigmaks2014@yandex.ru>
2024-04-12 12:38:53 +0300

    feat: CurrencyController implemented

--- src/CloudMining.App/Controllers/CurrencyController.cs
+++ src/CloudMining.App/Controllers/CurrencyController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CloudMining.Application.Models.Currencies;
+using CloudMining.Application.Services.Currencies;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CloudMining.App.Controllers
 {
     public class CurrencyController : Controller
     {
-        public IActionResult Index()
+        private readonly CurrencyService _currencyService;
+
+        public CurrencyController(CurrencyService currencyService)
         {
+            _currencyService = currencyService;
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddCurrency([FromBody]CurrencyDTO currency)
+        {
+            await _currencyService.CreateAsync(currency);
             return Ok();
         }
     }
