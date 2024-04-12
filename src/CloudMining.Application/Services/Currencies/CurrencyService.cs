8596b36225a3c39682f9e060af6c9b9f98da9fe7
solanova88 <grigmaks2014@yandex.ru>
2024-04-12 12:08:09 +0300

    feat: CreateAsync method in CurrencyService implemented

--- src/CloudMining.Application/Services/Currencies/CurrencyService.cs
+++ src/CloudMining.Application/Services/Currencies/CurrencyService.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CloudMining.Application.Models.Currencies;
+using CloudMining.Domain.Models;
+using CloudMining.Infrastructure.Database;
+
 
 namespace CloudMining.Application.Services.Currencies
 {
     public sealed class CurrencyService : ICurrencyService
     {
+        private readonly CloudMiningContext _context;
+
+        public CurrencyService(CloudMiningContext context)
+        {
+            _context = context;
+        }
+        public async Task CreateAsync(CurrencyDTO currency)
+        {
+            var newCurrency = new Currency()
+            {
+                Caption = currency.Caption,
+                Code = currency.Code,
+                Precision = currency.Precision
+            };
+            await _context.Currencies.AddAsync(newCurrency).ConfigureAwait(false);
+            await _context.SaveChangesAsync().ConfigureAwait(false);
+        }
     }
 }
--- src/CloudMining.Application/Services/Currencies/ICurrencyService.cs
+++ src/CloudMining.Application/Services/Currencies/ICurrencyService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CloudMining.Application.Models.Currencies;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace CloudMining.Application.Services.Currencies
 {
     public interface ICurrencyService
     {
-
+        Task CreateAsync(CurrencyDTO currency);
     }
 }
