bab6ca07e236ebf17eebbe906228bd6310b6d29a
solanova88 <grigmaks2014@yandex.ru>
2024-04-15 13:58:14 +0300

    feat: CreatePurchaseDto added

---
+++ src/CloudMining.Application/DTO/Payments/Purchase/CreatePurchaseDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudMining.Application.DTO.Payments.Purchase
+{
+    internal class CreatePurchaseDto
+    {
+    }
+}
