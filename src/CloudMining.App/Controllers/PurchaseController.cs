ca16bd38aae8765cf170e7878a236a3ba75de3ce
solanova88 <grigmaks2014@yandex.ru>
2024-04-15 14:55:29 +0300

    feat: PurchaseController added

---
+++ src/CloudMining.App/Controllers/PurchaseController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CloudMining.App.Controllers
+{
+    public class PurchaseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
