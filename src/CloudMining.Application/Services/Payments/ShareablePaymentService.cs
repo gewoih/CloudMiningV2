233891252f5e3ac1a7156072a9df9901be1f3486
Nikita Ranenko <nranenko@bk.ru>
2024-04-17 18:00:10 +0700

    feat: GetLatestPaymentDateAsync implemented

--- src/CloudMining.Application/Services/Payments/ShareablePaymentService.cs
+++ src/CloudMining.Application/Services/Payments/ShareablePaymentService.cs
@@ -4,6 +4,7 @@ using CloudMining.Application.Services.Shares;
 using CloudMining.Domain.Enums;
 using CloudMining.Domain.Models;
 using CloudMining.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 namespace CloudMining.Application.Services.Payments
 {
@@ -42,6 +43,17 @@ namespace CloudMining.Application.Services.Payments
 			await _context.SaveChangesAsync().ConfigureAwait(false);
 
 			return newPayment;
+		}
+
+		public async Task<DateTime> GetLatestPaymentDateAsync(PaymentType paymentType)
+		{
+			var latestPaymentDate = await _context.ShareablePayments
+				.Where(payment => payment.Type == paymentType)
+				.OrderByDescending(payment => payment.Date)
+				.Select(payment => payment.Date)
+				.FirstOrDefaultAsync();
+
+			return latestPaymentDate;
 		}
 	}
 }
