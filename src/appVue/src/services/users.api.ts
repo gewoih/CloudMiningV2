e32199a9a173391e0345b7a9b9a96d8b42835e36
Nikita Ranenko <nranenko@bk.ru>
2024-04-25 21:19:26 +0700

    refactor: removed excess files

--- src/appVue/src/services/users.api.ts
+++ src/appVue/src/services/users.api.ts
@@ -1,26 +1,26 @@
-import axios, { AxiosInstance } from 'axios';
-import { RegisterUser } from '@/models/RegisterUser';
+import axios, {AxiosInstance} from 'axios';
+import {RegisterUser} from '@/models/RegisterUser';
 import {LoginUser} from "@/models/LoginUser.ts";
 
 export default class UsersService {
-  private axiosInstance: AxiosInstance;
+    private axiosInstance: AxiosInstance;
 
-  constructor() {
-    this.axiosInstance = axios.create({
-      baseURL: 'https://localhost:5000/api',
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-  }
+    constructor() {
+        this.axiosInstance = axios.create({
+            baseURL: 'https://localhost:5000/api',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    }
+    
+    async createUser(userData: RegisterUser) {
+        return this.axiosInstance.post("/users", userData);
+    }
 
-  async createUser(userData: RegisterUser) {
-    return this.axiosInstance.post("/users", userData);
-  }
-  
-  async loginUser(userData: LoginUser){
-    return this.axiosInstance.post("/users/login", userData);
-  }
+    async loginUser(userData: LoginUser) {
+        return this.axiosInstance.post("/users/login", userData);
+    }
 }
 
 export const usersService = new UsersService();
