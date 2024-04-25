f9f8dba63b573b247c5b01e7826d99c7ad1db9a5
Nikita Ranenko <nranenko@bk.ru>
2024-04-25 23:15:55 +0700

    feat: bootstrap-vue-next modal implemented

--- src/appVue/src/main.ts
+++ src/appVue/src/main.ts
@@ -7,7 +7,7 @@ import {createBootstrap} from 'bootstrap-vue-next'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 
-createApp(App)
-    .use(router)
-    .use(createBootstrap)
-    .mount('#app');
+const app = createApp(App);
+app.use(router);
+app.use(createBootstrap());
+app.mount('#app');
