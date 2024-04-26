f561394c5de673cb9e81867e1b2d864047f02d5b
Nikita Ranenko <nranenko@bk.ru>
2024-04-26 15:24:20 +0700

    Revert "feat: bootstrap-vue-next modal implemented"
    This reverts commit f9f8dba63b573b247c5b01e7826d99c7ad1db9a5.

--- src/appVue/src/components/Payments.vue
+++ src/appVue/src/components/Payments.vue
@@ -6,9 +6,9 @@ import {CurrencyCode} from "@/enums/CurrencyCode.ts";
 import {PaymentType} from "@/enums/PaymentType.ts";
 
 const selectedPaymentType = ref(PaymentType.Electricity);
-const isModalShown = ref(false);
+// const isLoading = ref(false);
 const payments = ref<ShareablePayment[]>([]);
-const newPayment = ref<ShareablePayment>({
+const newPayment = ref<ShareablePayment>({ 
   caption: null,
   currencyCode: CurrencyCode.RUB,
   paymentType: PaymentType.Electricity,
@@ -16,16 +16,35 @@ const newPayment = ref<ShareablePayment>({
   amount: 0,
   isCompleted: false
 });
+const isModalOpen = ref(false);
 
 const fetchPayments = async () => {
+  // isLoading.value = true;
   payments.value = await shareablePaymentsService.getPayments(selectedPaymentType.value);
   console.log(payments.value);
+  // isLoading.value = false;
 };
 
+const showModal = () => {
+  isModalOpen.value = true;
+};
+
+const closeModal = () => {
+  isModalOpen.value = false;
+};
 const createPayment = async () => {
-  newPayment.value.paymentType = selectedPaymentType.value;
-  await shareablePaymentsService.createPayment(newPayment.value);
-  await fetchPayments();
+    newPayment.value.paymentType = selectedPaymentType.value;
+    await shareablePaymentsService.createPayment(newPayment.value);
+    await fetchPayments();
+    newPayment.value = { 
+      caption: null,
+      currencyCode: CurrencyCode.RUB,
+      paymentType: selectedPaymentType.value,
+      date: new Date(),
+      amount: 0,
+      isCompleted: false
+    };
+    closeModal();
 };
 
 fetchPayments();
@@ -33,6 +52,11 @@ fetchPayments();
 </script>
 
 <template>
+<!--  <div class="loading-container" v-if="isLoading">-->
+<!--    <div class="loading-spinner"></div>-->
+<!--    <p class="loading-message">Загрузка данных...</p>-->
+<!--  </div>-->
+
   <div class="container-fluid">
     <div class="row justify-content-end">
       <div class="col-auto">
@@ -54,10 +78,9 @@ fetchPayments();
             </tr>
             </thead>
             <tbody>
-            <tr v-for="payment in payments"
-                :class="{ 'table-success': payment.isCompleted, 'table-danger': !payment.isCompleted }">
+            <tr v-for="payment in payments" :class="{ 'table-success': payment.isCompleted, 'table-danger': !payment.isCompleted }">
               <td>{{ payment.amount }}</td>
-              <td>{{ payment.date }}</td>
+              <td>{{ new Date(payment.date).toLocaleDateString('ru-RU') }}</td>
               <td>{{ payment.caption }}</td>
             </tr>
             </tbody>
@@ -67,25 +90,40 @@ fetchPayments();
     </div>
     <div class="row justify-content-end">
       <div class="col-auto">
-        <button type="button" class="btn btn-primary m-3" v-on:click="isModalShown = !isModalShown">Добавить платеж</button>
+        <button type="button" class="btn btn-primary m-3" v-on:click="showModal()">Добавить платеж</button>
       </div>
     </div>
   </div>
 
   <!-- Modal -->
-  <BModal id="modal-center" centered title="Создание нового платежа" v-model="isModalShown">
-    <BForm @submit="createPayment">
-      <BFormGroup label="Название платежа" description="Укажите комментарий к платежу">
-        <BFormInput type="text" v-model="newPayment.caption" />
-      </BFormGroup>
-
-      <BFormGroup label="Дата">
-        <BFormInput type="date" v-model="newPayment.date" required/>
-      </BFormGroup>
-
-      <BFormGroup label="Сумма">
-        <BFormInput type="number" v-model="newPayment.amount" required/>
-      </BFormGroup>
-    </BForm>
-  </BModal>
+  <div class="modal fade" :class="{ 'show': isModalOpen }" id="exampleModal" tabindex="-1" aria-labelledby="addPaymentModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="addPaymentModalLabel">Добавление нового платежа</h5>
+          <button type="button" class="btn-close" aria-label="Close" @click="closeModal"></button>
+        </div>
+        <div class="modal-body">
+          <div class="mb-3" id="addPaymentForm">
+            <div class="form-group">
+              <label for="paymentCaption">Комментарий</label>
+              <input type="text" class="form-control" id="paymentCaption" v-model="newPayment.caption">
+            </div>
+            <div class="form-group">
+              <label for="paymentDate">Дата</label>
+              <input type="date" class="form-control" id="paymentDate" required v-model="newPayment.date">
+            </div>
+            <div class="form-group">
+              <label for="paymentAmount">Сумма</label>
+              <input type="number" class="form-control" id="paymentAmount" required v-model="newPayment.amount">
+            </div>
+          </div>
+        </div>
+        <div class="modal-footer border-top-0">
+          <button type="button" class="btn btn-secondary" @click="closeModal">Отмена</button>
+          <button type="button" class="btn btn-primary" @click="createPayment">Сохранить</button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
--- src/appVue/vite.config.ts
+++ src/appVue/vite.config.ts
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
-import Components from 'unplugin-vue-components/vite'
-import {BootstrapVueNextResolver} from 'unplugin-vue-components/resolvers'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), 
-    Components({
-    resolvers: [BootstrapVueNextResolver()],
-  })],
+  plugins: [vue()],
   server: {
     port: 8080
   },
