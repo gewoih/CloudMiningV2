24773e9c5c39667231d635465390b29904b6f429
Nikita Ranenko <nranenko@bk.ru>
2024-04-26 15:24:26 +0700

    Revert "build: bootstrap-vue-next installed"
    This reverts commit a09000d5ee72562a21fd0f7e5a8750c2b681d10b.

--- src/appVue/src/main.ts
+++ src/appVue/src/main.ts
@@ -2,12 +2,5 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router';
-import {createBootstrap} from 'bootstrap-vue-next'
 
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
-
-createApp(App)
-    .use(router)
-    .use(createBootstrap)
-    .mount('#app');
+createApp(App).use(router).mount('#app');
