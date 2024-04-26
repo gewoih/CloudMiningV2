f8d86fd7f318c08665946565820c1246b873aec1
Nikita Ranenko <nranenko@bk.ru>
2024-04-26 15:44:41 +0700

    feat: primeVue installed

--- src/appVue/src/main.ts
+++ src/appVue/src/main.ts
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from './router';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/aura-dark-amber/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+app.use(router);
+app.use(PrimeVue)
+app.mount('#app');
+
