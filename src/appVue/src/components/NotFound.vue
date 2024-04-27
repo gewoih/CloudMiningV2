b9f4cc695f923efa143a3edd6719715d62034e72
solanova88 <grigmaks2014@yandex.ru>
2024-04-27 15:40:48 +0300

    feat: NotFound.vue component implemented

---
+++ src/appVue/src/components/NotFound.vue
@@ -0,0 +1,11 @@
+﻿<script setup lang="ts">
+
+</script>
+
+<template>
+  <a class="flex align-items-center" style="font-size: 2.5rem">
+    <i class="pi pi-exclamation-triangle mr-4" style="font-size: 4rem"/>
+    <h1 class="text-center">404 Not Found</h1>
+  </a>
+</template>
+
