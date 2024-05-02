37d6c06c6f359ba13f6f04f7a629141da0c981a4
solanova88 <grigmaks2014@yandex.ru>
2024-05-02 15:02:40 +0300

    feat: Payment type selector implemented; style: Datatable modified

--- src/appVue/src/components/Payments.vue
+++ src/appVue/src/components/Payments.vue
@@ -1,30 +1,45 @@
 ﻿<template>
+<div class="w-8">
+  <Toolbar class="mb-4">
+    <template #start>
+      <Button label="New" icon="pi pi-plus" severity="success" class="mr-2" @click="" />
+    </template>
+
+    <template #end>
+      <Dropdown v-model="selectedPaymentType" :options="paymentTypes" optionLabel="name" optionValue="value" @change="fetchPayments" class="w-full md:w-14rem" />
+    </template>
+  </Toolbar>
   <DataTable :value="payments" dataKey="id">
-    <Column expander style="width: 5rem"/>
-    <Column field="caption" header="Комментарий"></Column>
-    <Column field="date" header="Дата">
-      <template #body="slotProps">
-        {{ getDateOnly(slotProps.data.date) }}
-      </template>
-    </Column>
-    <Column field="amount" header="Сумма"/>
+    <Column expander/>
     <Column field="isCompleted" header="Статус">
       <template #body="slotProps">
         <Tag :value="slotProps.data.isCompleted ? 'Завершен' : 'Ожидание'" :severity="getPaymentStatusSeverity(slotProps.data.isCompleted)" />
       </template>
     </Column>
+    <Column field="amount" header="Сумма"/>
+    <Column field="date" header="Дата">
+      <template #body="slotProps">
+        {{ getDateOnly(slotProps.data.date) }}
+      </template>
+    </Column>
+    <Column field="caption" header="Комментарий"></Column>
   </DataTable>
+</div>
 </template>
 
 <script setup lang="ts">
 import {ref} from 'vue';
 import {ShareablePayment} from "@/models/ShareablePayment.ts";
 import {paymentsService} from "@/services/payments.api.ts";
-import { format } from 'date-fns'
+import {format} from 'date-fns'
 import {CurrencyCode} from "@/enums/CurrencyCode.ts";
 import {PaymentType} from "@/enums/PaymentType.ts";
 
 const selectedPaymentType = ref(PaymentType.Electricity);
+const paymentTypes = ref([
+  { name: 'Электричество', value: 'Electricity' },
+  { name: 'Покупки', value: 'Purchase' }
+]);
 const payments = ref<ShareablePayment[]>([]);
 const newPayment = ref<ShareablePayment>({
   caption: null,
@@ -34,7 +49,6 @@ const newPayment = ref<ShareablePayment>({
   amount: 0,
   isCompleted: false
 });
-const isModalOpen = ref(false);
 
 const getPaymentStatusSeverity = (isCompleted: boolean) => {
    return isCompleted ? 'success' : 'danger';
@@ -48,13 +62,6 @@ const fetchPayments = async () => {
   payments.value = await paymentsService.getPayments(selectedPaymentType.value);
 };
 
-const showModal = () => {
-  isModalOpen.value = true;
-};
-
-const closeModal = () => {
-  isModalOpen.value = false;
-};
 const createPayment = async () => {
   newPayment.value.paymentType = selectedPaymentType.value;
   await paymentsService.createPayment(newPayment.value);
@@ -67,7 +74,6 @@ const createPayment = async () => {
     amount: 0,
     isCompleted: false
   };
-  closeModal();
 };
 
 fetchPayments();
