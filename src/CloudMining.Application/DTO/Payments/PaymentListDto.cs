7261b9ef2394764341b48da4c04400595dd2d27c
solanova88 <grigmaks2014@yandex.ru>
2024-05-06 14:37:12 +0300

    fix: Payments data type

--- src/CloudMining.Application/DTO/Payments/PaymentListDto.cs
+++ src/CloudMining.Application/DTO/Payments/PaymentListDto.cs
@@ -2,6 +2,6 @@
 
 public class PaymentListDto
 {
-    public List<PaymentDto> Payments { get; set; }
-    public int TotalCount { get; set; }
+    public IEnumerable<PaymentDto> Payments { get; set; }
+    public int TotalRecords { get; set; }
 }
