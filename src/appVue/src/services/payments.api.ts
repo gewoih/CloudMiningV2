88529a68b12404bc3ec3f0003e94848bdac4b4f4
solanova88 <grigmaks2014@yandex.ru>
2024-05-07 16:38:54 +0300

    feat: getShares method in payments.api.ts implemented

--- src/appVue/src/services/payments.api.ts
+++ src/appVue/src/services/payments.api.ts
@@ -2,6 +2,7 @@ import {ShareablePayment} from '@/models/ShareablePayment';
 import {PaymentType} from '@/enums/PaymentType';
 import {apiService} from '@/services/api.ts';
 import {ShareablePaymentList} from "@/models/ShareablePaymentList.ts";
+import {PaymentShare} from "@/models/PaymentShare.ts";
 
 class PaymentsService {
     async createPayment(paymentData: ShareablePayment) {
@@ -13,6 +14,12 @@ class PaymentsService {
         const response = await apiService.axiosInstance
             .get("/payments", {params: {paymentType, skip}});
         
+        return response.data;
+    }
+    async getShares(paymentId: string): Promise<PaymentShare[]> {
+        const response = await apiService.axiosInstance
+            .get("/payments/shares", {params: {paymentId}});
+
         return response.data;
     }
 }
