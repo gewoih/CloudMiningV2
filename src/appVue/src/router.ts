d21e6b4e0435764e9bec28b26bb35313e52674d0
solanova88 <grigmaks2014@yandex.ru>
2024-05-13 16:55:59 +0300

    feat: Payments component divided

--- src/appVue/src/router.ts
+++ src/appVue/src/router.ts
@@ -1,14 +1,16 @@
 import {createRouter, createWebHistory} from 'vue-router';
 import Registration from './components/Registration.vue';
 import Authentication from './components/Authentication.vue';
-import Payments from './components/Payments.vue';
 import NotFound from "@/components/NotFound.vue";
+import AdminPayments from "@/components/AdminPayments.vue";
+import Payments from "@/components/Payments.vue";
 
 const routes = [
     {path: '/user/register', name: 'register', component: Registration},
     {path: '/user/login', name: 'login', component: Authentication},
+    {path: '/admin/payments', name: 'adminPayments', component: AdminPayments},
     {path: '/payments', name: 'payments', component: Payments},
-    {path: "/:pathMatch(.*)", name: "NotFound", component: NotFound}
+    {path: "/:pathMatch(.*)", name: "notFound", component: NotFound}
 ];
 
 const router = createRouter({
