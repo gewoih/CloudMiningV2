24ce30ad17aa2969832f8d03bae8b94d8f10dfda
Nikita Ranenko <nranenko@bk.ru>
2024-05-14 22:20:19 +0800

    feat: CurrenciesController.cs removed

--- src/CloudMining.Application/Services/Currencies/ICurrencyService.cs
+++ src/CloudMining.Application/Services/Currencies/ICurrencyService.cs
@@ -1,12 +1,10 @@
-﻿using CloudMining.Application.DTO.Currencies;
-using CloudMining.Domain.Enums;
+﻿using CloudMining.Domain.Enums;
 using CloudMining.Domain.Models;
 
 namespace CloudMining.Application.Services.Currencies
 {
     public interface ICurrencyService
     {
-        Task<Currency> CreateAsync(CurrencyDto currency);
         Task<Currency?> GetAsync(CurrencyCode code);
         Task<Guid> GetIdAsync(CurrencyCode code);
     }
