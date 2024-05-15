1960be4f2f6567b56181d499d4c0f455346e2d1f
solanova88 <grigmaks2014@yandex.ru>
2024-05-15 17:35:15 +0300

    feat: user roles integrated in Payments.vue

--- src/appVue/src/components/Payments.vue
+++ src/appVue/src/components/Payments.vue
@@ -1,65 +1,93 @@
 ﻿<template>
-<div class="w-8">
-  
-  <Toolbar class="mb-4">
-    <template #start>
-      <Button label="Добавить платеж" icon="pi pi-plus" severity="success" class="mr-2" @click="isModalVisible = true" />
-    </template>
+  <div class="w-8">
+    <div :class="{'w-full flex justify-content-end mb-4': userRole !== UserRole.Admin}">
+      <Toolbar class="mb-4" v-if="userRole === UserRole.Admin">
+        <template #start>
+          <Button label="Добавить платеж" icon="pi pi-plus" severity="success" class="mr-2"
+                  @click="isModalVisible = true"/>
+        </template>
+        <template #end>
+          <Dropdown v-model="selectedPaymentType" :options="paymentTypes" optionLabel="name" optionValue="value"
+                    @change="fetchPayments" class="w-full md:w-14rem"/>
+        </template>
+      </Toolbar>
+      <Dropdown v-if="userRole !== UserRole.Admin" v-model="selectedPaymentType" :options="paymentTypes"
+                optionLabel="name" optionValue="value" @change="fetchPayments" class="w-full md:w-14rem"/>
+    </div>
 
-    <template #end>
-      <Dropdown v-model="selectedPaymentType" :options="paymentTypes" optionLabel="name" optionValue="value" @change="fetchPayments" class="w-full md:w-14rem" />
-    </template>
-  </Toolbar>
-  
-  <DataTable :value="payments" v-model:expandedRows="expandedRows" dataKey="id"
-             @rowExpand="fetchShares">
-    <Column expander/>
-    <Column field="isCompleted" header="Статус">
-      <template #body="slotProps">
-        <Tag :value="slotProps.data.isCompleted ? 'Завершен' : 'Ожидание'" :severity="getStatusSeverity(slotProps.data.isCompleted)" />
+
+    <DataTable v-if="userRole === UserRole.Admin" :value="payments" v-model:expandedRows="expandedRows" dataKey="id"
+               @rowExpand="fetchShares">
+      <Column expander />
+      <Column field="isCompleted" header="Статус">
+        <template #body="slotProps">
+          <Tag :value="slotProps.data.isCompleted ? 'Завершен' : 'Ожидание'"
+               :severity="getStatusSeverity(slotProps.data.isCompleted)"/>
+        </template>
+      </Column>
+      <Column field="amount" header="Сумма"/>
+      <Column field="date" header="Дата">
+        <template #body="slotProps">
+          {{ getDateOnly(slotProps.data.date) }}
+        </template>
+      </Column>
+      <Column field="caption" header="Комментарий"></Column>
+      <template #expansion="slotProps">
+        <div class="p-3">
+          <DataTable :value="paymentSharesMap[slotProps.data.id]">
+            <Column header="ФИО">
+              <template #body="slotProps">
+                {{
+                  slotProps.data.user.lastName + ' ' + slotProps.data.user.firstName + ' ' + slotProps.data.user.patronymic
+                }}
+              </template>
+            </Column>
+            <Column field="amount" header="Сумма"></Column>
+            <Column field="share" header="Доля"></Column>
+            <Column field="isCompleted" header="Статус">
+              <template #body="slotProps">
+                <Tag :value="slotProps.data.isCompleted ? 'Завершен' : 'Ожидание'"
+                     :severity="getStatusSeverity(slotProps.data.isCompleted)"/>
+              </template>
+            </Column>
+          </DataTable>
+        </div>
       </template>
-    </Column>
-    <Column field="amount" header="Сумма"/>
-    <Column field="date" header="Дата">
-      <template #body="slotProps">
-        {{ getDateOnly(slotProps.data.date) }}
-      </template>
-    </Column>
-    <Column field="caption" header="Комментарий"></Column>
-    <template #expansion="slotProps">
-      <div class="p-3">
-        <DataTable :value="paymentSharesMap[slotProps.data.id]">
-          <Column header="ФИО">
-            <template #body="slotProps">
-              {{ slotProps.data.user.lastName + ' ' + slotProps.data.user.firstName + ' ' + slotProps.data.user.patronymic }}
-            </template>
-          </Column>
-          <Column field="amount" header="Сумма" ></Column>
-          <Column field="share" header="Доля" ></Column>
-          <Column field="isCompleted" header="Статус">
-            <template #body="slotProps">
-              <Tag :value="slotProps.data.isCompleted ? 'Завершен' : 'Ожидание'" :severity="getStatusSeverity(slotProps.data.isCompleted)" />
-            </template>
-          </Column>
-        </DataTable>
-      </div>
-    </template>
-  </DataTable>
-  <Paginator :rows="pageSize" :totalRecords="totalPaymentsCount" :rowsPerPageOptions="[5, 10, 15]" @page="pageChange"></Paginator>
-</div>
-  
-  <Dialog v-model:visible="isModalVisible" modal header="Добавление платежа" :draggable="false" :dismissableMask="true">
+    </DataTable>
+    <DataTable v-if="userRole !== UserRole.Admin" :value="payments"  dataKey="id">
+      <Column field="isCompleted" header="Статус">
+        <template #body="slotProps">
+          <Tag :value="slotProps.data.isCompleted ? 'Завершен' : 'Ожидание'"
+               :severity="getStatusSeverity(slotProps.data.isCompleted)"/>
+        </template>
+      </Column>
+      <Column field="sharedAmount" header="Ваша сумма"/>
+      <Column field="share" header="Ваша доля"/>
+      <Column field="amount" header="Общаяя сумма"/>
+      <Column field="date" header="Дата">
+        <template #body="slotProps">
+          {{ getDateOnly(slotProps.data.date) }}
+        </template>
+      </Column>
+    </DataTable>
+    <Paginator :rows="pageSize" :totalRecords="totalPaymentsCount" :rowsPerPageOptions="[5, 10, 15]"
+               @page="pageChange"></Paginator>
+  </div>
+
+  <Dialog v-if="userRole === UserRole.Admin" v-model:visible="isModalVisible" modal header="Добавление платежа"
+          :draggable="false" :dismissableMask="true">
     <div class="flex align-items-center gap-3 mb-3">
       <label for="amount" class="font-semibold w-6rem">Сумма</label>
-      <InputNumber v-model="newPayment.amount" id="amount" class="flex-auto" autocomplete="off" />
+      <InputNumber v-model="newPayment.amount" id="amount" class="flex-auto" autocomplete="off"/>
     </div>
     <div class="flex align-items-center gap-3 mb-3">
       <label for="date" class="font-semibold w-6rem">Дата</label>
-      <Calendar id="date" v-model="newPayment.date" date-format="dd.mm.yy" show-icon class="flex-auto" autocomplete="off" />
+      <Calendar id="date" v-model="newPayment.date" date-format="dd.mm.yy" show-icon class="flex-auto"
+                autocomplete="off"/>
     </div>
     <div class="flex align-items-center gap-3 mb-5">
       <label for="caption" class="font-semibold w-6rem">Комментарий</label>
-      <InputText id="caption" v-model="newPayment.caption" class="flex-auto" autocomplete="off" />
+      <InputText id="caption" v-model="newPayment.caption" class="flex-auto" autocomplete="off"/>
     </div>
     <div class="flex justify-content-end gap-2">
       <Button type="submit" label="Сохранить" @click="createPayment"></Button>
@@ -73,7 +101,6 @@ import {paymentsService} from "@/services/payments.api.ts";
 import {format} from 'date-fns'
 import {CurrencyCode} from "@/enums/CurrencyCode.ts";
 import {PaymentType} from "@/enums/PaymentType.ts";
-import {Payment} from "@/models/Payment.ts";
 import {CreatePayment} from "@/models/CreatePayment.ts";
 import {PaymentShare} from "@/models/PaymentShare.ts";
 import {UserRole} from "@/enums/UserRole.ts";
@@ -89,6 +116,7 @@ const paymentShares = ref<PaymentShare[]>();
 const totalPaymentsCount = ref(0);
 const pageSize = ref(10);
 const pageNumber = ref(1);
+const userRole = ref(UserRole.User);
 
 const newPayment = ref<CreatePayment>({
   caption: null,
@@ -100,22 +128,28 @@ const newPayment = ref<CreatePayment>({
 });
 
 const paymentTypes = ref([
-  { name: 'Электричество', value: 'Electricity' },
-  { name: 'Покупки', value: 'Purchase' }
+  {name: 'Электричество', value: 'Electricity'},
+  {name: 'Покупки', value: 'Purchase'}
 ]);
 
 const pageChange = async (event) => {
-  pageNumber.value = event.page+1;
+  pageNumber.value = event.page + 1;
   pageSize.value = event.rows;
   await fetchPayments();
 }
 
 const getStatusSeverity = (isCompleted: boolean) => {
-   return isCompleted ? 'success' : 'danger';
+  return isCompleted ? 'success' : 'danger';
 }
 
 const getDateOnly = (date) => {
   return format(date, 'dd.MM.yyyy');
+};
+
+const getUserRole = () => {
+  const jwt = localStorage.getItem('access_token');
+  const decodedJwt = jwt && JSON.parse(atob(jwt.split('.')[1]));
+  userRole.value = decodedJwt["http://schemas.microsoft.com/ws/2008/06/identity/claims/role"];
 };
 
 const fetchShares = async (event) => {
@@ -148,7 +182,9 @@ const createPayment = async () => {
   isModalVisible.value = false;
 };
 
+getUserRole();
 fetchPayments();
+
 
 </script>
 
