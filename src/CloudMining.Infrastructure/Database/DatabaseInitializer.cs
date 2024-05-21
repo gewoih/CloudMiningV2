3f904c3c414bedf9f7cb1aceea44989ba0e48fdf
solanova88 <grigmaks2014@yandex.ru>
2024-05-21 14:06:55 +0300

    feat: Currencies Ids seeded, migration added

--- src/CloudMining.Infrastructure/Database/DatabaseInitializer.cs
+++ src/CloudMining.Infrastructure/Database/DatabaseInitializer.cs
@@ -15,12 +15,12 @@ namespace CloudMining.Infrastructure.Database
 		{
 			return
 			[
-				new() { Id = Guid.NewGuid(), Caption = "Рубль", Code = CurrencyCode.RUB, Precision = 2 },
-				new() { Id = Guid.NewGuid(), Caption = "Доллар", Code = CurrencyCode.USD, Precision = 2 },
-				new() { Id = Guid.NewGuid(), Caption = "Bitcoin", Code = CurrencyCode.BTC, Precision = 4 },
-				new() { Id = Guid.NewGuid(), Caption = "Etherium", Code = CurrencyCode.ETH, Precision = 4 },
-				new() { Id = Guid.NewGuid(), Caption = "Litecoin", Code = CurrencyCode.LTC, Precision = 2 },
-				new() { Id = Guid.NewGuid(), Caption = "Dogecoin", Code = CurrencyCode.DOGE, Precision = 0 }
+				new() { Id = new Guid("E23A58C6-9CEF-4C6F-94FC-8577A4B4FB84"), Caption = "Рубль", Code = CurrencyCode.RUB, Precision = 2 },
+				new() { Id = new Guid("F1DEBADF-A2C3-4908-A11C-8329DF252FB8"), Caption = "Доллар", Code = CurrencyCode.USD, Precision = 2 },
+				new() { Id = new Guid("9A3E4016-240E-44E3-A002-0EE2CFF499A3"), Caption = "Bitcoin", Code = CurrencyCode.BTC, Precision = 5 },
+				new() { Id = new Guid("8927702D-AE2E-4A5A-AF19-2E6FA1824648"), Caption = "Etherium", Code = CurrencyCode.ETH, Precision = 4 },
+				new() { Id = new Guid("62C520F6-499A-499E-86C2-B2AF9C8EBA42"), Caption = "Litecoin", Code = CurrencyCode.LTC, Precision = 2 },
+				new() { Id = new Guid("A5450179-3BFF-4645-9209-04ACC6168C5B"), Caption = "Dogecoin", Code = CurrencyCode.DOGE, Precision = 0 }
 			];
 		}
 
