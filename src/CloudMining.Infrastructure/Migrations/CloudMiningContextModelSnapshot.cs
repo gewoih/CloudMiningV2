07731ab0212c089090c9b49696d5d72617a3b440
solanova88 <grigmaks2014@yandex.ru>
2024-05-21 14:17:18 +0300

    feat: User avatar path marked as nullable

--- src/CloudMining.Infrastructure/Migrations/CloudMiningContextModelSnapshot.cs
+++ src/CloudMining.Infrastructure/Migrations/CloudMiningContextModelSnapshot.cs
@@ -59,7 +59,7 @@ namespace CloudMining.Infrastructure.Migrations
                     b.HasData(
                         new
                         {
-                            Id = new Guid("3b0ab7ba-a4aa-4697-9a87-7193f533fed0"),
+                            Id = new Guid("e23a58c6-9cef-4c6f-94fc-8577a4b4fb84"),
                             Caption = "Рубль",
                             Code = 1,
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
@@ -70,7 +70,7 @@ namespace CloudMining.Infrastructure.Migrations
                         },
                         new
                         {
-                            Id = new Guid("7984a97b-3fdc-4e3b-a85b-d05ad078ba08"),
+                            Id = new Guid("f1debadf-a2c3-4908-a11c-8329df252fb8"),
                             Caption = "Доллар",
                             Code = 0,
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
@@ -81,18 +81,18 @@ namespace CloudMining.Infrastructure.Migrations
                         },
                         new
                         {
-                            Id = new Guid("f624be73-447f-47ac-8911-0c72e07933c9"),
+                            Id = new Guid("9a3e4016-240e-44e3-a002-0ee2cff499a3"),
                             Caption = "Bitcoin",
                             Code = 2,
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             DeletedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
                             IsDeleted = false,
-                            Precision = 4,
+                            Precision = 5,
                             UpdatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified)
                         },
                         new
                         {
-                            Id = new Guid("24f28268-32bd-4ec9-b75d-60bbecaacec3"),
+                            Id = new Guid("8927702d-ae2e-4a5a-af19-2e6fa1824648"),
                             Caption = "Etherium",
                             Code = 3,
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
@@ -103,7 +103,7 @@ namespace CloudMining.Infrastructure.Migrations
                         },
                         new
                         {
-                            Id = new Guid("44de677d-e090-4cd4-ae64-eac0681c0866"),
+                            Id = new Guid("62c520f6-499a-499e-86c2-b2af9c8eba42"),
                             Caption = "Litecoin",
                             Code = 4,
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
@@ -114,7 +114,7 @@ namespace CloudMining.Infrastructure.Migrations
                         },
                         new
                         {
-                            Id = new Guid("002fd265-9afc-4de6-9e43-e1a58e760ff6"),
+                            Id = new Guid("a5450179-3bff-4645-9209-04acc6168c5b"),
                             Caption = "Dogecoin",
                             Code = 5,
                             CreatedDate = new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
@@ -162,7 +162,6 @@ namespace CloudMining.Infrastructure.Migrations
                         .HasColumnType("integer");
 
                     b.Property<string>("AvatarPath")
-                        .IsRequired()
                         .HasColumnType("text");
 
                     b.Property<string>("ConcurrencyStamp")
