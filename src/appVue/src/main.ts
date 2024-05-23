fb4f7304018980937a63f4e5bd8febdbad358931
Nikita Ranenko <nranenko@bk.ru>
2024-05-23 19:22:22 +0800

    refactor

--- src/appVue/src/main.ts
+++ src/appVue/src/main.ts
@@ -9,8 +9,6 @@ import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css';
 
-
-
 const app = createApp(App);
 app.use(router);
 app.use(PrimeVue)
