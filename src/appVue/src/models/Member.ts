7b0dbfe356098be7b131f568bc7bea92562528af
solanova88 <grigmaks2014@yandex.ru>
2024-05-28 18:48:28 +0300

    feat: Member.ts and MemberDeposit.ts implemented

---
+++ src/appVue/src/models/Member.ts
@@ -0,0 +1,12 @@
+﻿export interface Member{
+    user: {
+        id: string;
+        firstName: string;
+        lastName: string;
+        patronymic: string;
+    };
+    id: string;
+    share: number;
+    amount: number;
+    date: Date;
+}
