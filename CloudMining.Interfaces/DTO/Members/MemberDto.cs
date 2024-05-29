6f0993982ec846c9033c23216cb9b5ece62c8cfd
solanova88 <grigmaks2014@yandex.ru>
2024-05-29 13:55:01 +0300

    feat: MemberDto.cs implemented

---
+++ CloudMining.Interfaces/DTO/Members/MemberDto.cs
@@ -0,0 +1,6 @@
+﻿using CloudMining.Interfaces.DTO.Currencies;
+using CloudMining.Interfaces.DTO.Users;
+
+namespace CloudMining.Interfaces.DTO.Members;
+
+public record MemberDto(UserDto User, decimal Amount, decimal Share, CurrencyDto Currency, DateTime RegistrationDate);
