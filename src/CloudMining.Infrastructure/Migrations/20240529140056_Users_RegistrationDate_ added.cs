9f8203dfc0a3f11b5ea34edc933ee06f175c1b78
solanova88 <grigmaks2014@yandex.ru>
2024-05-29 17:37:26 +0300

    feat: RegistrationDate field added, migrations added

---
+++ src/CloudMining.Infrastructure/Migrations/20240529140056_Users_RegistrationDate_ added.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CloudMining.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Users_RegistrationDate_added : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RegistrationDate",
+                table: "AspNetUsers",
+                type: "timestamp with time zone",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RegistrationDate",
+                table: "AspNetUsers");
+        }
+    }
+}
