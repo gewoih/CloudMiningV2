3c1bba199fe978e7a9b113fdca10795e2149d3e5
solanova88 <grigmaks2014@yandex.ru>
2024-05-29 17:38:43 +0300

    feat: finding first deposit date added

--- src/CloudMining.Application/Services/DepositService.cs
+++ src/CloudMining.Application/Services/DepositService.cs
@@ -1,5 +1,6 @@
 ﻿using CloudMining.Application.Mappings;
 using CloudMining.Domain.Enums;
+using CloudMining.Domain.Models.Identity;
 using CloudMining.Domain.Models.Payments;
 using CloudMining.Infrastructure.Database;
 using CloudMining.Interfaces.DTO.Payments.Deposits;
@@ -10,50 +11,62 @@ namespace CloudMining.Application.Services;
 
 public sealed class DepositService : IDepositService
 {
-	private readonly CloudMiningContext _context;
-	private readonly ICurrencyService _currencyService;
-	private readonly IMapper<Deposit, CreateDepositDto> _depositMapper;
-	private readonly IShareService _shareService;
+    private readonly CloudMiningContext _context;
+    private readonly ICurrencyService _currencyService;
+    private readonly IMapper<Deposit, CreateDepositDto> _depositMapper;
+    private readonly IShareService _shareService;
 
-	public DepositService(CloudMiningContext context, IShareService shareService, ICurrencyService currencyService,
-		IMapper<Deposit, CreateDepositDto> depositMapper)
-	{
-		_context = context;
-		_shareService = shareService;
-		_currencyService = currencyService;
-		_depositMapper = depositMapper;
-	}
+    public DepositService(CloudMiningContext context, IShareService shareService, ICurrencyService currencyService,
+        IMapper<Deposit, CreateDepositDto> depositMapper)
+    {
+        _context = context;
+        _shareService = shareService;
+        _currencyService = currencyService;
+        _depositMapper = depositMapper;
+    }
 
-	public async Task<List<Deposit>> GetUserDeposits(Guid userId)
-	{
-		return await _context.Deposits.Where(deposit => deposit.UserId == userId).ToListAsync();
-	}
+    public async Task<List<Deposit>> GetUserDeposits(Guid userId)
+    {
+        return await _context.Deposits.Where(deposit => deposit.UserId == userId).ToListAsync();
+    }
 
-	public async Task<Deposit> AddDepositAndRecalculateShares(CreateDepositDto depositDto)
-	{
-		var currencyId = await _currencyService.GetIdAsync(CurrencyCode.RUB);
-		var deposit = _depositMapper.ToDomain(depositDto);
-		deposit.CurrencyId = currencyId;
+    public async Task<Deposit> AddDepositAndRecalculateShares(CreateDepositDto depositDto)
+    {
+        var currencyId = await _currencyService.GetIdAsync(CurrencyCode.RUB);
+        var deposit = _depositMapper.ToDomain(depositDto);
+        deposit.CurrencyId = currencyId;
 
-		var usersDeposits = await GetUsersDeposits();
-		usersDeposits[depositDto.UserId] += depositDto.Amount;
+        var firstDeposit = await _context.Deposits
+            .Where(firstDeposit => firstDeposit.UserId == deposit.UserId)
+            .OrderBy(firstDeposit => firstDeposit.Date)
+            .FirstOrDefaultAsync()
+            .ConfigureAwait(false);
+        var firstDepositDate = firstDeposit?.Date;
+        var user = await _context.Users.FindAsync(deposit.UserId);
+        if (user != null && (firstDepositDate == null || deposit.Date < firstDepositDate))
+        {
+            user.RegistrationDate = deposit.Date;
+        }
 
-		var newShareChanges = await _shareService.GetUpdatedUsersSharesAsync(usersDeposits, depositDto.Date);
-		deposit.ShareChanges = newShareChanges;
+        var usersDeposits = await GetUsersDeposits();
+        usersDeposits[depositDto.UserId] += depositDto.Amount;
 
-		await _context.Deposits.AddAsync(deposit);
-		await _context.SaveChangesAsync();
+        var newShareChanges = await _shareService.GetUpdatedUsersSharesAsync(usersDeposits, depositDto.Date);
+        deposit.ShareChanges = newShareChanges;
 
-		return deposit;
-	}
+        await _context.Deposits.AddAsync(deposit);
+        await _context.SaveChangesAsync();
 
-	private async Task<Dictionary<Guid, decimal>> GetUsersDeposits()
-	{
-		var usersDeposits = await _context.Users
-			.Include(user => user.Deposits)
-			.ToDictionaryAsync(group => group.Id, group => group.Deposits.Sum(deposit => deposit.Amount))
-			.ConfigureAwait(false);
+        return deposit;
+    }
 
-		return usersDeposits;
-	}
+    private async Task<Dictionary<Guid, decimal>> GetUsersDeposits()
+    {
+        var usersDeposits = await _context.Users
+            .Include(user => user.Deposits)
+            .ToDictionaryAsync(group => group.Id, group => group.Deposits.Sum(deposit => deposit.Amount))
+            .ConfigureAwait(false);
+
+        return usersDeposits;
+    }
 }
