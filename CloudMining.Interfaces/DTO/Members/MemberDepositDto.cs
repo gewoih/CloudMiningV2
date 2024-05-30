232edfde271c9696712d7d3877d927d073928ec1
solanova88 <grigmaks2014@yandex.ru>
2024-05-30 14:44:56 +0300

    feat: caption is nullable in MemberDepositDto.cs

--- CloudMining.Interfaces/DTO/Members/MemberDepositDto.cs
+++ CloudMining.Interfaces/DTO/Members/MemberDepositDto.cs
@@ -1,3 +1,3 @@
 ﻿namespace CloudMining.Interfaces.DTO.Members;
 
-public record MemberDepositDto(Guid Id, DateTime Date, decimal Amount, string Caption);
+public record MemberDepositDto(Guid Id, DateTime Date, decimal Amount, string? Caption);
