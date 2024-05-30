481f9045b24c56fd9e98d4627bc9f5cbfdb9eeb6
solanova88 <grigmaks2014@yandex.ru>
2024-05-30 13:55:56 +0300

    feat: registration date is nullable

--- CloudMining.Interfaces/DTO/Members/MemberDto.cs
+++ CloudMining.Interfaces/DTO/Members/MemberDto.cs
@@ -3,4 +3,4 @@ using CloudMining.Interfaces.DTO.Users;
 
 namespace CloudMining.Interfaces.DTO.Members;
 
-public record MemberDto(UserDto User, decimal Amount, decimal Share, CurrencyDto Currency, DateTime RegistrationDate);
+public record MemberDto(UserDto User, decimal Amount, decimal? Share, DateTime? RegistrationDate);
