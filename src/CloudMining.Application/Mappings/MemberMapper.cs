51acf612ea2ab8151a2a2c05ebae8cad49ff847e
Nikita Ranenko <nranenko@bk.ru>
2024-05-31 18:05:37 +0800

    feat: MemberMapper simplified

--- src/CloudMining.Application/Mappings/MemberMapper.cs
+++ src/CloudMining.Application/Mappings/MemberMapper.cs
@@ -1,6 +1,4 @@
-﻿using CloudMining.Domain.Enums;
-using CloudMining.Domain.Models.Identity;
-using CloudMining.Interfaces.DTO.Currencies;
+﻿using CloudMining.Domain.Models.Identity;
 using CloudMining.Interfaces.DTO.Members;
 using CloudMining.Interfaces.DTO.Users;
 using CloudMining.Interfaces.Interfaces;
@@ -9,16 +7,20 @@ namespace CloudMining.Application.Mappings;
 
 public class MemberMapper : IMapper<User, MemberDto>
 {
+    private readonly IShareService _shareService;
+
+    public MemberMapper(IShareService shareService)
+    {
+        _shareService = shareService;
+    }
+
     public MemberDto ToDto(User model)
     {
-        var currentMemberShare =
-            model.ShareChanges
-                .MaxBy(shareChange => shareChange.CreatedDate)
-                ?.After;
-        var totalAmount = model.Deposits
-            .Sum(deposit => deposit.Amount);
+        var userShare = _shareService.CalculateUserShare(model.ShareChanges);
+        var totalAmount = model.Deposits.Sum(deposit => deposit.Amount);
         var user = new UserDto(model.Id, model.FirstName, model.LastName, model.Patronymic);
-        return new MemberDto(user, totalAmount, currentMemberShare, model.RegistrationDate);
+        
+        return new MemberDto(user, totalAmount, userShare, model.RegistrationDate);
     }
 
     public User ToDomain(MemberDto dto)
