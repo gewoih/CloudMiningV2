c80268abbfc6115f4c989cec286b1e9009e824e5
solanova88 <grigmaks2014@yandex.ru>
2024-06-18 17:22:31 +0300

    feat: payment amount cannot be negative

--- src/CloudMining.Api/Validators/Payment/PaymentValidator.cs
+++ src/CloudMining.Api/Validators/Payment/PaymentValidator.cs
@@ -9,9 +9,14 @@ public class PaymentValidator : AbstractValidator<CreatePaymentDto>
 	public PaymentValidator()
 	{
 		RuleFor(x => x.Amount)
+			.GreaterThan(0)
+			.WithMessage("Сумма платежа не может быть отрицательной");
+		
+		RuleFor(x => x.Amount)
 			.GreaterThanOrEqualTo(100)
 			.When(x => x.CurrencyCode == CurrencyCode.RUB)
 			.WithMessage("Сумма платежа не может быть меньше 100");
+		
 		RuleFor(x => x.Date)
 			.GreaterThanOrEqualTo(new DateTime(2021, 6, 1))
 			.WithMessage("Дата платежа не может быть раньше создания проекта");
