6bb19212471c39fe9030768785009b081c59061e
solanova88 <grigmaks2014@yandex.ru>
2024-06-21 15:47:46 +0300

    refactor

--- src/CloudMining.Infrastructure/Settings/EmcdSettings.cs
+++ src/CloudMining.Infrastructure/Settings/EmcdSettings.cs
@@ -5,11 +5,11 @@ public class EmcdSettings
 	public static readonly string SectionName = "Emcd";
 	public string ApiKey { get; set; }
 	public string BaseUrl { get; set; }
-	public Endpoints Endpoints { get; set; }
+	public EmcdEndpoints Endpoints { get; set; }
 	public List<string> AvailableCoins { get; set; }
 }
 
-public class Endpoints
+public class EmcdEndpoints
 {
 	public string GetPayoutsUrl { get; set; }
 }
