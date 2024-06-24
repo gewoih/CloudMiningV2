4674ae538185cbdf28ae3a80797ae0764c1880ff
solanova88 <grigmaks2014@yandex.ru>
2024-06-24 13:35:49 +0300

    feat: EnumUtils.cs implemented

---
+++ src/CloudMining.Domain/Utils/EnumUtils.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace CloudMining.Application.Utils;
+
+public static class EnumUtils
+{
+    public static string GetDescription(this Enum e)
+    {
+        return e.GetType()
+            .GetMember(e.ToString())
+            .FirstOrDefault()?
+            .GetCustomAttribute<DescriptionAttribute>()?
+            .Description ?? e.ToString();
+    }
+}
