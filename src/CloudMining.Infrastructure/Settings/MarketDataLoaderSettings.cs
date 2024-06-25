ec684aa4480532cef716c2344d96d07025d64b68
solanova88 <grigmaks2014@yandex.ru>
2024-06-25 18:49:53 +0300

    feat: MarketDataLoaderSettings.cs implemented

--- src/CloudMining.Infrastructure/Settings/MarketDataLoaderSettings.cs
+++ src/CloudMining.Infrastructure/Settings/MarketDataLoaderSettings.cs
@@ -1,7 +1,16 @@
-﻿namespace CloudMining.Infrastructure.Settings;
+﻿using CloudMining.Domain.Enums;
+
+namespace CloudMining.Infrastructure.Settings;
 
 public class MarketDataLoaderSettings
 {
     public static readonly string SectionName = "MarketDataLoader";
     public int DelayInMinutes { get; set; }
+    public List<CurrencyPairs> CurrencyPairs { get; set; }
+}
+
+public class CurrencyPairs
+{
+    public CurrencyCode From { get; set; }
+    public CurrencyCode To { get; set; }
 }
