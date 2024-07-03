37afe42885a4f07296d4ca1a79774613bce4cedd
solanova88 <grigmaks2014@yandex.ru>
2024-07-03 22:21:07 +0300

    feat: CbrSettings.cs implemented

---
+++ src/CloudMining.Infrastructure/Settings/CbrSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace CloudMining.Infrastructure.Settings;
+
+public class CbrSettings
+{
+    public static readonly string SectionName = "Cbr";
+    public string BaseUrl { get; set; }
+    public string UsdCode { get; set; }
+    public CbrEndpoints Endpoints { get; set; }
+}
+
+public class CbrEndpoints
+{
+    public string GetHistoricalPriceDataUrl { get; set; }
+    public string GetDailyPriceDataUrl { get; set; }
+}
