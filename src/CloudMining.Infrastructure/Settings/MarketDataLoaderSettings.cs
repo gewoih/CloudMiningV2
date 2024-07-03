479bb6fafed1e30c4a2112dfaf1924645629e231
solanova88 <grigmaks2014@yandex.ru>
2024-07-03 22:22:36 +0300

    feat: Added prop CbrDelay

--- src/CloudMining.Infrastructure/Settings/MarketDataLoaderSettings.cs
+++ src/CloudMining.Infrastructure/Settings/MarketDataLoaderSettings.cs
@@ -6,6 +6,7 @@ public sealed class MarketDataLoaderSettings
 {
     public static readonly string SectionName = "MarketDataLoader";
     public TimeSpan Delay { get; set; }
+    public TimeSpan CbrDelay { get; set; }
     public List<CurrencyPair> CurrencyPairs { get; set; }
 }
 
