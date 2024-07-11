bacc1ffc1d70a674318e58e6e84fd38fa4aa1c19
solanova88 <grigmaks2014@yandex.ru>
2024-07-11 12:46:38 +0300

    feat: HomeController.cs added

---
+++ src/CloudMining.Api/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using CloudMining.Interfaces.DTO.Statistics;
+using CloudMining.Interfaces.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CloudMining.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+[Authorize]
+public class HomeController : ControllerBase
+{
+    private readonly IStatisticsService _statisticsService;
+
+    public HomeController(IStatisticsService statisticsService)
+    {
+        _statisticsService = statisticsService;
+    }
+    
+    [HttpGet]
+    public async Task<HomePageDto> Get()
+    {
+
+        var statisticList = await _statisticsService.GetStatisticsListAsync();
+        var homePageDto = new HomePageDto(statisticList);
+        return homePageDto;
+
+    }
+
+}
