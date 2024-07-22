a47127c3b1957a472fb8cc11448ab1183c0b21ea
solanova88 <grigmaks2014@yandex.ru>
2024-07-22 13:22:29 +0300

    refactor

--- src/CloudMining.Application/Services/MarketDataLoaderService.cs
+++ src/CloudMining.Application/Services/MarketDataLoaderService.cs
@@ -9,67 +9,59 @@ namespace CloudMining.Application.Services;
 
 public sealed class MarketDataLoaderService : BackgroundService
 {
-	private readonly TimeSpan _loadingDelay;
+    private readonly TimeSpan _loadingDelay;
 
-	//TODO: Вынести параметры с датами в appsettings
-	private readonly DateTime _loadHistoricalDataFrom = new(2020, 1, 1, 0, 0, 0, DateTimeKind.Utc);
-	private readonly DateTime _loadHistoricalDataTo = DateTime.UtcNow;
-	private readonly IServiceScopeFactory _scopeFactory;
-	private readonly List<CurrencyPair> _currencyPairs;
-	private readonly IMarketDataLoaderStrategyFactory _marketDataLoaderStrategyFactory;
+    //TODO: Вынести параметры с датами в appsettings
+    private readonly DateTime _loadHistoricalDataFrom = new(2020, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+    private readonly DateTime _loadHistoricalDataTo = DateTime.UtcNow;
+    private readonly IServiceScopeFactory _scopeFactory;
+    private readonly List<CurrencyPair> _currencyPairs;
 
-	public MarketDataLoaderService(IOptions<MarketDataLoaderSettings> settings,
-		IServiceScopeFactory scopeFactory,
-		IMarketDataLoaderStrategyFactory marketDataLoaderStrategyFactory)
-	{
-		_scopeFactory = scopeFactory;
-		_marketDataLoaderStrategyFactory = marketDataLoaderStrategyFactory;
-		_loadingDelay = settings.Value.Delay;
-		_currencyPairs = settings.Value.CurrencyPairs;
-	}
+    public MarketDataLoaderService(IOptions<MarketDataLoaderSettings> settings,
+        IServiceScopeFactory scopeFactory)
+    {
+        _scopeFactory = scopeFactory;
+        _loadingDelay = settings.Value.Delay;
+        _currencyPairs = settings.Value.CurrencyPairs;
+    }
 
-	protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-	{
-		var loadOnlyRealtimeData = false;
-		while (!stoppingToken.IsCancellationRequested)
-		{
-			await using var scope = _scopeFactory.CreateAsyncScope();
-			var marketDataService = scope.ServiceProvider.GetRequiredService<IMarketDataService>();
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            await using var scope = _scopeFactory.CreateAsyncScope();
+            var marketDataService = scope.ServiceProvider.GetRequiredService<IMarketDataService>();
 
-			await LoadAndSaveMarketData(marketDataService, loadOnlyRealtimeData);
-			loadOnlyRealtimeData = true;
+            await LoadAndSaveMarketData(marketDataService);
 
-			await Task.Delay(_loadingDelay, stoppingToken);
-		}
-	}
+            await Task.Delay(_loadingDelay, stoppingToken);
+        }
+    }
 
-	private async Task LoadAndSaveMarketData(IMarketDataService marketDataService, bool loadOnlyRealtimeData)
-	{
-		foreach (var currencyPair in _currencyPairs)
-		{
-			var loadDataFrom = _loadHistoricalDataFrom;
-			if (loadOnlyRealtimeData)
-			{
-				var lastMarketDataDate =
-					await marketDataService.GetLastMarketDataDateAsync(currencyPair.From, currencyPair.To);
+    private async Task LoadAndSaveMarketData(IMarketDataService marketDataService)
+    {
+        foreach (var currencyPair in _currencyPairs)
+        {
+            var lastMarketDataDate =
+                await marketDataService.GetLastMarketDataDateAsync(currencyPair.From, currencyPair.To) ??
+                _loadHistoricalDataFrom;
 
-				if (lastMarketDataDate.HasValue)
-					loadDataFrom = lastMarketDataDate.Value;
-			}
+            var marketData = await GetMarketDataAsync(currencyPair, lastMarketDataDate, _loadHistoricalDataTo);
+            await marketDataService.SaveMarketDataAsync(marketData);
+        }
+    }
 
-			var marketData = await GetMarketDataAsync(currencyPair, loadDataFrom, _loadHistoricalDataTo);
-			await marketDataService.SaveMarketDataAsync(marketData);
-		}
-	}
+    private async Task<List<MarketData>> GetMarketDataAsync(CurrencyPair currencyPair,
+        DateTime? fromDate = null,
+        DateTime? toDate = null)
+    {
+        await using var scope = _scopeFactory.CreateAsyncScope();
+        var marketDataLoaderStrategyFactory =
+            scope.ServiceProvider.GetRequiredService<IMarketDataLoaderStrategyFactory>();
+        var marketDataLoaderStrategy = marketDataLoaderStrategyFactory.Create(currencyPair);
+        var marketData =
+            await marketDataLoaderStrategy.GetMarketDataAsync(currencyPair, fromDate, toDate);
 
-	private async Task<List<MarketData>> GetMarketDataAsync(CurrencyPair currencyPair,
-		DateTime? fromDate = null,
-		DateTime? toDate = null)
-	{
-		var marketDataLoaderStrategy = _marketDataLoaderStrategyFactory.Create(currencyPair);
-		var marketData =
-			await marketDataLoaderStrategy.GetMarketDataAsync(currencyPair, fromDate, toDate);
-
-		return marketData;
-	}
+        return marketData;
+    }
 }
