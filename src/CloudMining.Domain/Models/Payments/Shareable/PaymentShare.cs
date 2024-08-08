624795e4bc7f7d93106c7e6230facb54500d64da
Nikita Ranenko <nranenko@bk.ru>
2024-08-08 22:00:55 +0700

    feat: Date removed from PaymentShare, ETH payouts added in DatabaseInitializer.cs

--- src/CloudMining.Domain/Models/Payments/Shareable/PaymentShare.cs
+++ src/CloudMining.Domain/Models/Payments/Shareable/PaymentShare.cs
@@ -9,7 +9,6 @@ public class PaymentShare : Entity
 	public User User { get; set; }
 	public Guid UserId { get; set; }
 	public Guid ShareablePaymentId { get; set; }
-	public DateTime Date { get; set; }
 	public decimal Amount { get; set; }
 	public decimal Share { get; set; }
 	public ShareStatus Status { get; set; }
--- src/CloudMining.Interfaces/Interfaces/IShareService.cs
+++ src/CloudMining.Interfaces/Interfaces/IShareService.cs
@@ -11,5 +11,5 @@ public interface IShareService
 	Task<List<UserShare>> GetUsersSharesAsync();
 	decimal CalculateUserShare(List<ShareChange> shareChanges);
 	Task<List<ShareChange>> GetUpdatedUsersSharesAsync(Dictionary<Guid, decimal> usersDeposits, DateTime newDepositDate);
-	Task<List<PaymentShare>> CreatePaymentShares(decimal amount, Currency currency, DateTime date);
+	Task<List<PaymentShare>> CreatePaymentShares(decimal amount, Currency currency);
 }
