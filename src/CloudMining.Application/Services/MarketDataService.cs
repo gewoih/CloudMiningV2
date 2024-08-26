a4f8e2b49761a036a544de9643b11c6a2d35cd45
solanova88 <grigmaks2014@yandex.ru>
2024-08-26 16:48:35 +0300

    feat: receive and sell strategy implemented

--- src/CloudMining.Application/Services/MarketDataService.cs
+++ src/CloudMining.Application/Services/MarketDataService.cs
@@ -9,70 +9,133 @@ namespace CloudMining.Application.Services;
 
 public sealed class MarketDataService : IMarketDataService
 {
-    private readonly CloudMiningContext _context;
+	private readonly CloudMiningContext _context;
 
-    public MarketDataService(CloudMiningContext context)
-    {
-        _context = context;
-    }
+	public MarketDataService(CloudMiningContext context)
+	{
+		_context = context;
+	}
 
-    public async Task SaveMarketDataAsync(List<MarketData> marketData)
-    {
-        var existingCombinations = await _context.MarketData
-            .GroupBy(data => new { data.From, data.To })
-            .Select(group => new { group.Key.From, group.Key.To, MaxDate = group.Max(x => x.Date)})
-            .ToListAsync();
+	public async Task SaveMarketDataAsync(List<MarketData> marketData)
+	{
+		var existingCombinations = await _context.MarketData
+			.GroupBy(data => new { data.From, data.To })
+			.Select(group => new { group.Key.From, group.Key.To, MaxDate = group.Max(x => x.Date) })
+			.ToListAsync();
 
-        var existingCombinationsHashSet = existingCombinations
-            .Select(data => (data.From, data.To, data.MaxDate))
-            .ToHashSet();
+		var existingCombinationsHashSet = existingCombinations
+			.Select(data => (data.From, data.To, data.MaxDate))
+			.ToHashSet();
 
-        foreach (var data in marketData)
-        {
-            var combo = (data.From, data.To, data.Date);
-            if (!existingCombinationsHashSet.Contains(combo))
-                await _context.MarketData.AddAsync(data);
-        }
+		foreach (var data in marketData)
+		{
+			var combo = (data.From, data.To, data.Date);
+			if (!existingCombinationsHashSet.Contains(combo))
+				await _context.MarketData.AddAsync(data);
+		}
 
-        await _context.SaveChangesAsync()
-            .ConfigureAwait(false);
-    }
+		await _context.SaveChangesAsync()
+			.ConfigureAwait(false);
+	}
 
-    public async Task<DateTime?> GetLastMarketDataDateAsync(CurrencyCode fromCurrency, CurrencyCode toCurrency)
-    {
-        var lastMarketDataDate = await _context.MarketData
-            .Where(marketData => marketData.From == fromCurrency && marketData.To == toCurrency)
-            .MaxAsync(marketData => (DateTime?)marketData.Date);
+	public async Task<DateTime?> GetLastMarketDataDateAsync(CurrencyCode fromCurrency, CurrencyCode toCurrency)
+	{
+		var lastMarketDataDate = await _context.MarketData
+			.Where(marketData => marketData.From == fromCurrency && marketData.To == toCurrency)
+			.MaxAsync(marketData => (DateTime?)marketData.Date);
 
-        return lastMarketDataDate;
-    }
+		return lastMarketDataDate;
+	}
 
-    public async Task<Dictionary<CurrencyPair, MarketData?>> GetLatestMarketDataForCurrenciesAsync(IEnumerable<CurrencyPair> currencyPairs)
-    {
-        var result = new Dictionary<CurrencyPair, MarketData?>();
-        
-        foreach (var currencyPair in currencyPairs)
-        {
-            var latestMarketData = await _context.MarketData
-                .Where(marketData => marketData.From == currencyPair.From && marketData.To == currencyPair.To)
-                .OrderByDescending(marketData => marketData.Date)
-                .FirstOrDefaultAsync();
-            
-            result[currencyPair] = latestMarketData;
-        }
+	public async Task<Dictionary<CurrencyPair, MarketData?>> GetLatestMarketDataForCurrenciesAsync(
+		IEnumerable<CurrencyPair> currencyPairs)
+	{
+		var result = new Dictionary<CurrencyPair, MarketData?>();
 
-        return result;
-    }
+		foreach (var currencyPair in currencyPairs)
+		{
+			var latestMarketData = await _context.MarketData
+				.Where(marketData => marketData.From == currencyPair.From && marketData.To == currencyPair.To)
+				.OrderByDescending(marketData => marketData.Date)
+				.FirstOrDefaultAsync();
 
-    public async Task<decimal> GetLastUsdToRubRateAsync(
-        CurrencyCode from = CurrencyCode.USD,
-        CurrencyCode to = CurrencyCode.RUB)
-    {
-        var usdToRubRate = await _context.MarketData
-            .Where(marketData => marketData.From == from && marketData.To == to)
-            .OrderByDescending(marketData => marketData.Date)
-            .Select(marketData => marketData.Price)
-            .FirstOrDefaultAsync();
-        return usdToRubRate;
-    }
+			result[currencyPair] = latestMarketData;
+		}
+
+		return result;
+	}
+
+	public async Task<Dictionary<CurrencyPair, List<MarketData?>>> GetMarketDataForCurrenciesByDateAsync(
+		IEnumerable<CurrencyPair> currencyPairs, List<DateTime> requiredDates)
+	{
+		var result = new Dictionary<CurrencyPair, List<MarketData?>>();
+		
+		var requiredDateTimes = requiredDates
+			.Select(d => d.Date.AddHours(d.Hour))
+			.Distinct()
+			.ToList();
+
+		var marketDataList = await _context.MarketData
+			.Where(marketData => 
+				requiredDateTimes.Contains(marketData.Date.Date.AddHours(marketData.Date.Hour)))
+			.ToListAsync();
+
+		foreach (var currencyPair in currencyPairs)
+		{
+			var marketDataListByDates = new List<MarketData?>();
+
+			var data = marketDataList
+				.Where(marketData => marketData.From == currencyPair.From && marketData.To == currencyPair.To)
+				.OrderByDescending(marketData => marketData.Date)
+				.ToList();
+
+			marketDataListByDates.AddRange(data);
+
+			result[currencyPair] = marketDataListByDates;
+		}
+
+		return result;
+	}
+
+	public async Task<decimal> GetLastUsdToRubRateAsync(
+		CurrencyCode from = CurrencyCode.USD,
+		CurrencyCode to = CurrencyCode.RUB)
+	{
+		var usdToRubRate = await _context.MarketData
+			.Where(marketData => marketData.From == from && marketData.To == to)
+			.OrderByDescending(marketData => marketData.Date)
+			.Select(marketData => marketData.Price)
+			.FirstOrDefaultAsync();
+		return usdToRubRate;
+	}
+
+	public async Task<Dictionary<DateTime, decimal>> GetUsdToRubRatesByDateAsync(
+		List<DateTime> payoutsDates,
+		CurrencyCode from = CurrencyCode.USD,
+		CurrencyCode to = CurrencyCode.RUB)
+	{
+		var usdToRubRatesByDate = new Dictionary<DateTime, decimal>();
+
+		var marketDataList = await _context.MarketData
+			.Where(marketData =>
+				marketData.From == from &&
+				marketData.To == to &&
+				payoutsDates.Select(d => d.Date)
+					.Any(date => marketData.Date.Date <= date))
+			.OrderByDescending(marketData => marketData.Date)
+			.ToListAsync();
+
+		foreach (var date in payoutsDates)
+		{
+			var usdToRubRate = marketDataList
+				.Where(marketData => marketData.Date.Date <= date.Date)
+				.OrderByDescending(marketData => marketData.Date)
+				.Select(marketData => marketData.Price)
+				.FirstOrDefault();
+
+			usdToRubRatesByDate[date.Date] = usdToRubRate;
+		}
+
+		return usdToRubRatesByDate;
+	}
 }
--- src/CloudMining.Application/Services/ReceiveAndSellCalculationStrategy.cs
+++ src/CloudMining.Application/Services/ReceiveAndSellCalculationStrategy.cs
@@ -1,12 +1,250 @@
-﻿using CloudMining.Interfaces.DTO.Statistics;
+﻿using CloudMining.Domain.Enums;
+using CloudMining.Domain.Models.Currencies;
+using CloudMining.Domain.Models.Payments.Shareable;
+using CloudMining.Infrastructure.Settings;
+using CloudMining.Interfaces.DTO.Currencies;
+using CloudMining.Interfaces.DTO.Statistics;
 using CloudMining.Interfaces.Interfaces;
+using Microsoft.Extensions.Options;
 
 namespace CloudMining.Application.Services;
 
 public class ReceiveAndSellCalculationStrategy : IStatisticsCalculationStrategy
 {
-    public Task<StatisticsDto> GetStatisticsAsync()
-    {
-        throw new NotImplementedException();
-    }
+	private readonly DateTime _currentDate = DateTime.UtcNow;
+	private readonly IMarketDataService _marketDataService;
+	private readonly int _monthsSinceProjectStartDate;
+	private readonly DateTime _projectStartDate;
+	private readonly IShareablePaymentService _shareablePaymentService;
+
+
+	public ReceiveAndSellCalculationStrategy(IShareablePaymentService shareablePaymentService,
+		IOptions<ProjectInformationSettings> projectInformation,
+		IMarketDataService marketDataService)
+	{
+		_shareablePaymentService = shareablePaymentService;
+		_marketDataService = marketDataService;
+		_projectStartDate = DateTime.SpecifyKind(projectInformation.Value.ProjectStartDate, DateTimeKind.Utc);
+		_monthsSinceProjectStartDate = CalculateMonthsSinceProjectStart();
+	}
+
+	public async Task<StatisticsDto> GetStatisticsAsync()
+	{
+		var payoutsList =
+			await _shareablePaymentService.GetAsync(paymentTypes: [PaymentType.Crypto], includePaymentShares: false);
+		var payoutsDates = GetPayoutsDates(payoutsList);
+		var usdToRubRatesByDate = await _marketDataService.GetUsdToRubRatesByDateAsync(payoutsDates);
+		var incomes = await GetPriceBarsAsync(payoutsList, usdToRubRatesByDate, payoutsDates);
+		var totalIncome = incomes.Sum(priceBar => priceBar.Value);
+		var monthlyIncome = totalIncome / _monthsSinceProjectStartDate;
+		var expenses = await _shareablePaymentService.GetAsync(
+			paymentTypes: [PaymentType.Electricity, PaymentType.Purchase],
+			includePaymentShares: false);
+		var spentOnElectricity = expenses.Where(payment => payment.Type == PaymentType.Electricity)
+			.Sum(payment => payment.Amount);
+		var spentOnPurchases = expenses.Where(payment => payment.Type == PaymentType.Purchase)
+			.Sum(payment => payment.Amount);
+		var totalExpense = spentOnElectricity + spentOnPurchases;
+		var totalProfit = totalIncome - totalExpense;
+		var monthlyProfit = totalProfit / _monthsSinceProjectStartDate;
+		var paybackPercent = totalExpense != 0 ? totalProfit / totalExpense * 100 : 0;
+		var expensesList = GetExpenses(expenses);
+		var profits = GetProfitsList(incomes, expensesList);
+
+		var statisticsDto = new StatisticsDto(
+			totalIncome,
+			monthlyIncome,
+			totalExpense,
+			spentOnElectricity,
+			spentOnPurchases,
+			totalProfit,
+			monthlyProfit,
+			paybackPercent,
+			incomes,
+			profits,
+			expensesList);
+
+		return statisticsDto;
+	}
+
+	private int CalculateMonthsSinceProjectStart()
+	{
+		var totalMonths = (_currentDate.Year - _projectStartDate.Year) * 12 + _currentDate.Month -
+		                  _projectStartDate.Month;
+		if (_currentDate.Day < _projectStartDate.Day)
+			totalMonths--;
+
+		return totalMonths;
+	}
+
+	private async Task<List<MonthlyPriceBar>> GetPriceBarsAsync(
+		List<ShareablePayment> payouts,
+		Dictionary<DateTime, decimal> usdToRubRate,
+		List<DateTime> payoutsDates)
+	{
+		var uniqueCurrencyPairs = payouts
+			.Select(payment => new CurrencyPair { From = payment.Currency.Code, To = CurrencyCode.USDT })
+			.Distinct()
+			.ToList();
+
+		var currencyRates =
+			await _marketDataService.GetMarketDataForCurrenciesByDateAsync(uniqueCurrencyPairs, payoutsDates);
+
+		var usdIncomeByDate = GetUsdIncomeByDate(payouts, currencyRates);
+		var rubIncomeByDate = GetRubIncomeByDate(usdIncomeByDate, usdToRubRate);
+
+		var priceBars = new List<MonthlyPriceBar>();
+
+		foreach (var (date, incomeValue) in rubIncomeByDate)
+		{
+			priceBars.Add(
+				new MonthlyPriceBar(incomeValue, new DateOnly(date.Year, date.Month, 1)));
+		}
+
+		return priceBars;
+	}
+
+
+	private static Dictionary<DateTime, decimal> GetUsdIncomeByDate(
+		List<ShareablePayment> payouts,
+		Dictionary<CurrencyPair, List<MarketData?>> currencyRates,
+		CurrencyCode toCurrency = CurrencyCode.USDT)
+	{
+		var usdIncomeByDate = new Dictionary<DateTime, decimal>();
+
+		foreach (var payout in payouts)
+		{
+			foreach (var (currencyPair, marketDataList) in currencyRates)
+			{
+				if (payout.Currency.Code != currencyPair.From || currencyPair.To != toCurrency) continue;
+				
+				var matchingMarketData = marketDataList
+					.Find(marketData =>
+						marketData != null &&
+						payout.Date.Date.AddHours(payout.Date.Hour) == marketData.Date);
+
+				if (matchingMarketData == null) continue;
+				
+				var incomeInUsd = payout.Amount * matchingMarketData.Price;
+
+				usdIncomeByDate[payout.Date] = incomeInUsd;
+			}
+		}
+
+		return usdIncomeByDate;
+	}
+
+	private static Dictionary<DateTime, decimal> GetRubIncomeByDate(Dictionary<DateTime, decimal> usdIncome,
+		IReadOnlyDictionary<DateTime, decimal> usdToRubRate)
+	{
+		var rubIncome = new Dictionary<DateTime, decimal>();
+
+		foreach (var (dateTime, incomeValue) in usdIncome)
+		{
+			var date = dateTime.Date;
+
+			if (!usdToRubRate.TryGetValue(date, out var rate)) continue;
+			
+			if (rubIncome.ContainsKey(date))
+			{
+				rubIncome[date] += incomeValue * rate;
+			}
+			else
+			{
+				rubIncome[date] = incomeValue * rate;
+			}
+		}
+
+		return rubIncome;
+	}
+
+
+	private List<Expense> GetExpenses(List<ShareablePayment> payments)
+	{
+		var expenseList = new List<Expense>();
+		var electricityPayments = payments.Where(payment => payment.Type == PaymentType.Electricity);
+		var purchases = payments.Where(payment => payment.Type == PaymentType.Purchase);
+
+		var electricityMonthlyPriceBars = CalculateMonthlyExpenses(electricityPayments);
+		var purchasesMonthlyPriceBars = CalculateMonthlyExpenses(purchases);
+
+		var totalMonthlyPriceBars = electricityMonthlyPriceBars.Concat(purchasesMonthlyPriceBars);
+		var generalExpensePriceBars = CalculateGeneralMonthlyExpenses(totalMonthlyPriceBars);
+
+		expenseList.Add(new Expense(ExpenseType.OnlyElectricity, electricityMonthlyPriceBars));
+		expenseList.Add(new Expense(ExpenseType.OnlyPurchases, purchasesMonthlyPriceBars));
+		expenseList.Add(new Expense(ExpenseType.Total, generalExpensePriceBars));
+
+		return expenseList;
+	}
+
+	private List<MonthlyPriceBar> CalculateMonthlyExpenses(IEnumerable<ShareablePayment> payments)
+	{
+		var monthlyExpenses = payments
+			.GroupBy(payment => (payment.Date.Year, payment.Date.Month))
+			.ToList();
+
+		var priceBars = new List<MonthlyPriceBar>();
+		var processingDate = _projectStartDate;
+
+		while (processingDate <= _currentDate)
+		{
+			var totalAmount = monthlyExpenses
+				.Where(expense =>
+					expense.Key.Year == processingDate.Year &&
+					expense.Key.Month == processingDate.Month)
+				.Sum(expense => expense.Sum(payment => payment.Amount));
+
+			if (totalAmount != 0)
+				priceBars.Add(new MonthlyPriceBar(totalAmount,
+					new DateOnly(processingDate.Year, processingDate.Month, 1)));
+
+			processingDate = processingDate.AddMonths(1);
+		}
+
+		return priceBars;
+	}
+
+	private static List<MonthlyPriceBar> CalculateGeneralMonthlyExpenses(IEnumerable<MonthlyPriceBar> monthlyPriceBars)
+	{
+		var generalExpensePriceBars = monthlyPriceBars
+			.GroupBy(priceBar => priceBar.Date)
+			.Select(group => new MonthlyPriceBar(
+				group.Sum(priceBar => priceBar.Value),
+				group.Key
+			))
+			.ToList();
+
+		return generalExpensePriceBars;
+	}
+
+	private static List<MonthlyPriceBar> GetProfitsList(List<MonthlyPriceBar> incomes, IEnumerable<Expense> expenses)
+	{
+		var generalExpenses = expenses
+			.Where(expense => expense.Type == ExpenseType.Total)
+			.SelectMany(expense => expense.PriceBars)
+			.ToList();
+
+		if (generalExpenses.Count == 0)
+			return incomes;
+
+		var profits = incomes
+			.Concat(generalExpenses.Select(expense => expense with { Value = -expense.Value }))
+			.GroupBy(priceBar => priceBar.Date)
+			.Select(group => new MonthlyPriceBar(
+				group.Sum(priceBar => priceBar.Value),
+				group.Key))
+			.ToList();
+
+		return profits;
+	}
+
+	private static List<DateTime> GetPayoutsDates(IEnumerable<ShareablePayment> payouts)
+	{
+		var payoutsDates = payouts
+			.Select(payment => payment.Date)
+			.Distinct()
+			.ToList();
+		return payoutsDates;
+	}
 }
