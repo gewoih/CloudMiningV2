be676f2f4f019c95528dbd2597c07095a5124c7c
Nikita Ranenko <nranenko@bk.ru>
2024-10-25 15:21:12 +0200

    feat: cors allow any

--- src/CloudMining.Api/Startup/CorsSetup.cs
+++ src/CloudMining.Api/Startup/CorsSetup.cs
@@ -7,9 +7,9 @@ public static class CorsSetup
 		services.AddCors(options =>
 		{
 			options.AddPolicy("AllowSpecificOrigin", configure =>
-				configure.WithOrigins("http://localhost:8080", "http://localhost")
+				configure.AllowAnyHeader()
 					.AllowAnyMethod()
-					.AllowAnyHeader());
+					.AllowAnyOrigin());
 		});
 
 		return services;
